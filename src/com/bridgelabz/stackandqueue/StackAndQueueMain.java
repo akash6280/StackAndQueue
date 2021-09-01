c02a625d57efafd713d5c76ad03049844fdf1cc1
Akash Singh <akashsingh6280@gmail.com>
2021-09-01 06:47:39 +0530

    ADDED: function call to push() to create a stack

---
+++ src/com/bridgelabz/stackandqueue/StackAndQueueMain.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.stackandqueue;
+
+public class StackAndQueueMain{
+	public static void main(String[] args) {
+	
+		Stack<Integer> stack = new Stack<Integer>();
+		Node<Integer> firstNode = new Node<Integer>(70);
+		Node<Integer> secondNode = new Node<Integer>(30);
+		Node<Integer> thirdNode = new Node<Integer>(56);
+		
+		stack.push(firstNode);
+		stack.push(secondNode);
+		stack.push(thirdNode);
+		stack.printStack();
+		
+	}
+
+}
