1c612dbd92cd8c9aed44d7fecb7c4d01a9974329
Akash Singh <akashsingh6280@gmail.com>
2021-09-01 07:17:20 +0530

    ADDED: function call for enqueue operation

--- src/com/bridgelabz/stackandqueue/StackAndQueueMain.java
+++ src/com/bridgelabz/stackandqueue/StackAndQueueMain.java
@@ -3,16 +3,15 @@ package com.bridgelabz.stackandqueue;
 public class StackAndQueueMain{
 	public static void main(String[] args) {
 	
-		Stack<Integer> stack = new Stack<Integer>();
+		Queue<Integer> queue = new Queue<Integer>();
 		Node<Integer> firstNode = new Node<Integer>(70);
 		Node<Integer> secondNode = new Node<Integer>(30);
 		Node<Integer> thirdNode = new Node<Integer>(56);
 		
-		stack.push(firstNode);
-		stack.push(secondNode);
-		stack.push(thirdNode);
-		System.out.println(stack.peek());
-		stack.pop();
+		queue.enqueue(firstNode);
+		queue.enqueue(secondNode);
+		queue.enqueue(thirdNode);
+		queue.printQueue();
 		
 	}
 
